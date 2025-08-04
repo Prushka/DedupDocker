7cce0861f1845da45f3e44c53c4fd079a8037348
Dan Lyu <dan@lyu.sh>
2025-08-05 00:39:40 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 docker buildx build --platform linux/amd64 -f Dockerfile \
  --tag meinya/dedup:$(git describe --tags --dirty --always) \
  --tag meinya/dedup:latest --push .
+
+echo $(git describe --tags --dirty --always)
